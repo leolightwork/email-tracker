815235073d5c7c83ba4271fec491e24d1d8023fb
leolightwork <808sk8cake@gmail.com>
2025-11-29 22:45:03 -0600

    Final UI Touches, Basic react file structure setup

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, { use } from 'react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import MainCardHeader from './MainCardHeader';
 import '../styles/main-card.css';
 
 const MainCard = () => {
-  const form = useForm();
+  const { register, handleSubmit } = useForm();
+  const [view, setView] = useState('create');
+  const [email, setEmail] = useState('');
+  const [course, setSubject] = useState('');
+  const [message, setMessage] = useState();
+  const [date, setDate] = useState('');
+  const [interval, setInterval] = useState(0);
 
   return (
     <>
-      <div className="maincard-wrapper">
-        <div className="form-wrapper">
-          <div className="maincard-header-wrapper">
-            <h2>Email Submission Form</h2>
-            <MainCardHeader />
-          </div>
+      <div className="body-wrapper">
+        {/* <div className="card-wraper"> */}
+        <div className="card-header">
+          <button>Create</button>
+          <button>Email List</button>
+        </div>
 
-          <form>
-          </form>
+        <div className="form-wrapper">
+          <div className="banner-create">
+            <h3>Create</h3>
+          </div>
+          <div className="main-form">
+            <form onSubmit={handleSubmit}>
+              <div className="label-flex">
+                <label htmlFor="email">Email:</label>
+                <input {...register('email')} />
+              </div>
+
+              <div className="label-flex">
+                <label htmlFor="course">Course:</label>
+                <input {...register('course')} />
+              </div>
+              
+              <h4>Schedule / Interval </h4>
+              <div className="label-flex">
+                <label htmlFor="date">Date To Send:</label>
+                <input {...register('date')} />
+              </div>
+              <div className="label-flex">
+                <label htmlFor="interval">Days Of Each Recurring Submission:</label>
+                <input {...register('interval')} />
+              </div>
+              <button type='submit' className='form-button'>Create</button>
+            </form>
+          </div>
         </div>
       </div>
+      {/* </div> */}
     </>
   );
 };
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from './Header.jsx';
 import MainCard from './MainCard.jsx';
-import '../styles/global.css'
-
+import '../styles/global.css';
+import '../styles/header.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
+  
+    <div className="nav-wrapper">
+      <h1>Email Submission Interface</h1>
+    </div>
+
     <MainCard />
-    
   </StrictMode>
 );
